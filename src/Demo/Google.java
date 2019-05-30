b63e0ed050de870438203f410bc98b4e0a65e79c
Manikandan Rajendiran <manikandanrj@mavericsystems.lan>
2019-05-30 14:10:38 +0530

    Fisrt Commit

---
+++ src/Demo/Google.java
@@ -0,0 +1,18 @@
+package Demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Google {
+	
+	public static void main(String[] args) {
+		
+		System.setProperty("webdriver.gecko.driver", ".\\drivers\\geckodriver.exe");
+
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/?");
+		
+	}
+
+}
